7c7131aaef2cdce580ae11a5b71f2592aa7a3532
Richa Jaishwal <richajaiswalhome@gmail.com>
2024-11-14 12:03:55 +0530

    added files

---
+++ summarize.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const axios = require('axios');
+
+async function summarizeText(text){
+let data =JSON.stringify({
+  "inputs":text,
+  "parameters":{
+    "max_length":100,
+    "min_length":30
+  }
+});
+
+  let config = {
+    method: 'post',
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization':`Bearer ${ process.env.ACCESS_TOKEN}`
+
+    },
+    data : data
+  };
+
+    try {
+      const response = await axios.request(config); 
+      return response.data[0].summary_text;
+    }
+    catch (err) {
+      console.log(err);
+      return "There was an error processing the request.";
+    }
+} 
+
+
+module.exports=summarizeText;
